3db3c6a2e44cdf5e879eea7a7d22148505123cc6
Roman Polulikh <roman.polulikh@mycubes.nl>
2022-02-10 18:20:14 +0200

    second one

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -1,15 +1,28 @@
 package com.example.demo.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.HelloWorldBean;
 
 @RestController
 public class DemoController {
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping
 	public HelloWorldBean test() {
 		return new HelloWorldBean("Hello world");
+	}
+	
+	@GetMapping("/hello-int")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("hello.world.message", null, "Default message", LocaleContextHolder.getLocale());
 	}
 }
